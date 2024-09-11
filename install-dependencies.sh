963b4a239f917809d230e81136eb344fd1c6a20f
fuddydev <nerdcat.plays@gmail.com>
2024-09-11 16:12:32 +0200

    Updated userIntefaceProcSetup.cpp and removed userInterface.cpp

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -4,3 +4,4 @@ sudo apt install -y build-essential libx11-dev libxft-dev libxext-dev g++ cmake 
 sudo apt install -y gdb valgrind
 sudo apt install -y libjpeg-dev libpng-dev libfreetype6-dev
 sudo apt install -y pkg-config
+sudo apt install libfltk1.3-dev
