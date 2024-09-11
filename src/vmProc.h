6c275dca131404ce6efd006ff13c9e35b510bf7c
fuddydev <nerdcat.plays@gmail.com>
2024-09-11 19:17:45 +0200

    Added functionality to vmProc.h

--- src/vmProc.h
+++ src/vmProc.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <string>
+#include <FL/fl_ask.H>
 class AndroidVM {
 public:
-    AndroidVM(int version, const std::string& name);
+    AndroidVM(int version, const std::string& name) {
+        fl_alert("Andromeda Virtual Machine is starting a new process, do not shut down Andromeda!");
+    };
 private:
     int version_;
     std::string name_;
