1052e3a4d52d2533e503e1e02fd0117b62a33507
fuddydev <nerdcat.plays@gmail.com>
2024-09-11 14:52:05 +0200

    Fixed install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 sudo apt update
-sudo apt install build-essential libx11-dev libxft-dev libxext-dev
+sudo apt install -y build-essential libx11-dev libxft-dev libxext-dev g++ cmake git
+sudo apt install -y gdb valgrind
+sudo apt install -y libjpeg-dev libpng-dev libfreetype6-dev
+sudo apt install -y pkg-config
