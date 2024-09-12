0da6b9790c2b42d29bf189cb3dbbd601a2115b62
fuddydev <nerdcat.plays@gmail.com>
2024-09-12 05:58:15 +0200

    Added VM barebones

--- src/userInterface.cpp
+++ src/userInterface.cpp
@@ -2,8 +2,10 @@
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Box.H>
+#include "vmProc.h"
 int main() {
     Fl_Window* window = new Fl_Window(340, 180, "Andromeda Process Setup");
+    AndroidVM android(1, "Andromeda VM");
     window->end();
     window->show();
     return Fl::run();
--- src/vmProc.h
+++ src/vmProc.h
@@ -4,9 +4,12 @@
 class AndroidVM {
 public:
     AndroidVM(int version, const std::string& name) {
-        fl_alert("Andromeda Virtual Machine is starting a new process, do not shut down Andromeda!");
+        initVM();
     };
 private:
+    int initVM() {
+        fl_alert("Andromeda Virtual Machine is starting a new process, do not shut down Andromeda!");
+    };
     int version_;
     std::string name_;
 };
